bafd1666785c13ad936aa6232e08c38d28004a47
PatchapongKul <patchapong.kul@gmail.com>
2022-09-19 17:18:55 +0700

    read temperature and display on serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <Wire.h>
+#include <Arduino_HTS221.h>
 #include <NTPClient.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -6,8 +8,16 @@
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
 
+TwoWire sensor(1);
+bool a = sensor.setPins(41, 40);
+HTS221Class HTS1(sensor);
+
+QueueHandle_t tempQueue;
+
 void updateNTP( void * parameter );
 void displaySerial( void * parameter );
+void readTemp( void * parameter );
+void displayTemp( void * parameter );
 
 void setup(){
   Serial.begin(115200);
@@ -22,6 +32,14 @@ void setup(){
   Serial.println("WiFi Connected");
   delay(500);
   
+  if (!HTS1.begin()) {
+    Serial.println("Failed to initialize humidity temperature sensor!");
+    while (1);
+  }
+  Serial.println("Initialize Temperature Sensor ..... Done!");
+
+  tempQueue = xQueueCreate( 10, sizeof(float));
+
   timeClient.begin();
   timeClient.update();
   timeClient.setTimeOffset(7*3600); //In Thailand (UTC+7)
@@ -31,12 +49,28 @@ void setup(){
   "updateNTP",  /* name of task. */
   10000,        /* Stack size of task */
   NULL,         /* parameter of the task */
-  2,            /* priority of the task */
+  3,            /* priority of the task */
   NULL);        /* Task handle to keep track of created task */
 
   xTaskCreate(
   displaySerial,    /* Task function. */
   "displaySerial",  /* name of task. */
+  10000,            /* Stack size of task */
+  NULL,             /* parameter of the task */
+  2,                /* priority of the task */
+  NULL);            /* Task handle to keep track of created task */
+
+  xTaskCreate(
+  readTemp,    /* Task function. */
+  "readTemp",  /* name of task. */
+  10000,            /* Stack size of task */
+  NULL,             /* parameter of the task */
+  1,                /* priority of the task */
+  NULL);            /* Task handle to keep track of created task */
+
+  xTaskCreate(
+  displayTemp,    /* Task function. */
+  "displayTemp",  /* name of task. */
   10000,            /* Stack size of task */
   NULL,             /* parameter of the task */
   1,                /* priority of the task */
@@ -66,5 +100,33 @@ void displaySerial( void * parameter )
   {
     Serial.println(timeClient.getFormattedTime());
     vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+}
+
+void readTemp( void * parameter )
+{
+  for(;;)
+  {
+  float temperature = HTS1.readTemperature();
+  xQueueSend( tempQueue, ( void * ) &temperature, ( TickType_t ) 0 );
+  vTaskDelay(6000/portTICK_PERIOD_MS);
+  }
+}
+
+void displayTemp( void * parameter )
+{
+  for(;;)
+  {
+    float display_temp;
+    if( tempQueue != NULL )
+      {
+        while( xQueueReceive( tempQueue, &( display_temp ),( TickType_t ) 10 ) == pdPASS )
+        {
+          Serial.print("Temperature = ");
+          Serial.print(display_temp);
+          Serial.println("°C");
+        }
+      }
+    vTaskDelay(20000/portTICK_PERIOD_MS);
   }
 }
