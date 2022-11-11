e2cf84ab2f5959020b19b23206bea5e637dc1847
Rewlgil <rewlgil@hotmail.com>
2022-11-11 15:42:59 +0700

    publish mock data to netpie

---
+++ .pio/build/espressif32/FrameworkArduino/firmware_msc_fat.c.d
@@ -0,0 +1,3 @@
+.pio/build/espressif32/FrameworkArduino/firmware_msc_fat.c.o: \
+ C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.c \
+ C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/firmware_msc_fat.h
