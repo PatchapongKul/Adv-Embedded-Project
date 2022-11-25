4cc353af774effb2c2998882aaa91bfeb8f1bf1e
Rewlgil <rewlgil@hotmail.com>
2022-11-25 12:38:06 +0700

    remove .pio

--- .pio/build/espressif32/FrameworkArduino/cbuf.cpp.d
+++
@@ -1,3 +0,0 @@
-.pio/build/espressif32/FrameworkArduino/cbuf.cpp.o: \
- C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
- C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
--- .pio/build/espressif32/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/espressif32/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/rewlg/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
