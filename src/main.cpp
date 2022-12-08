384153fc66f91563407b5d1cd61ffe839acbad57
Rewlgil <rewlgil@hotmail.com>
2022-12-09 01:00:02 +0700

    add PIR sensor and subscribe MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,30 @@
 #include <PubSubClient.h>
 #include "secrets.h"
 
+#define LEDpin 2
+#define PIRpin 5
+#define HoldOnMillis 2000
+#define SentIntervalMillis 10000
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 
+hw_timer_t *timer0 = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+portMUX_TYPE synch = portMUX_INITIALIZER_UNLOCKED;
+
 // NETPIE Configuration
 const char* mqtt_server = "broker.netpie.io";
-String serverPath = "https://ds.netpie.io/feed/api/v1/datapoints/query";
 const int mqtt_port = 1883;
 long lastMsg = 0; char msg[50]; int value = 0;
 
 void sendPIR( void * parameter );
 void reconnect();
+void callback(char* topic, byte* payload, unsigned int length);
+void IRAM_ATTR PIRtrig(void);
+void IRAM_ATTR onTimer();
+
+bool PIRdetected = false, stateChange = false;
 
 void setup() {
   Serial.begin(115200);
@@ -30,39 +43,76 @@ void setup() {
   // MQTT Connection
   Serial.println("connecting to MQTT");
   client.setServer(mqtt_server, mqtt_port);
-  client.connect(clientID, token, secret);
-  delay(100);
+  reconnect();
 
-  xTaskCreate(sendPIR, "sendPIR", 4000, NULL, 1, NULL);
+  pinMode(LEDpin, OUTPUT);
+  pinMode(PIRpin, INPUT);
+  attachInterrupt(PIRpin, PIRtrig, HIGH);
+
+  timer0 = timerBegin(0, 8000, true);
+  timerAttachInterrupt(timer0, &onTimer, true);
+  timerAlarmWrite(timer0, HoldOnMillis * 10, false);
+  timerAlarmEnable(timer0);
+
+  xTaskCreate(sendPIR, "sendPIR", 8000, NULL, 1, NULL);
 }
 
-void loop() {
+void loop() 
+{
+  if (client.connected()) {
+    client.loop();
+  } else {
+    reconnect();
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) 
+{
+  String msg;
+  for (int i = 0; i < length; i++) {
+    msg = msg + (char)payload[i];
+  }
+  
+  if (String(topic) == "@msg/led") {
+    if (msg == "on") {
+      digitalWrite(LEDpin, LOW);
+    } else if (msg == "off") {
+      digitalWrite(LEDpin, HIGH);
+    }
+  }
 }
 
 void sendPIR( void * parameter )
 {
+  long lastSentMilllis = 0, CurrentMillis;
+
   for(;;)
   {
-    int sensorValue = (random(0,2));
-    Serial.print("Sensor Value: ");
-    Serial.println(sensorValue);
-
-    String sent_data = "{\"data\":{\"PIR\":";
-    sent_data += String(sensorValue);
-    sent_data += "}}";
-
-    if (sent_data.length() > 1)
+    CurrentMillis = millis();
+    if ((CurrentMillis - lastSentMilllis) > SentIntervalMillis || stateChange)
     {
-      while (!client.connected()) 
-      {
+      // Serial.print("Sensor Value: ");
+      // Serial.println(PIRdetected);
+
+      String sent_data = "{\"data\":{\"PIR\":";
+      sent_data += String(PIRdetected);
+      sent_data += ",\"LED\":";
+      sent_data += String(digitalRead(LEDpin));
+      sent_data += "}}";
+        
+      while (!client.connected()) {
         reconnect();
       }
       
-      client.publish("@shadow/data/update", sent_data.c_str());
+      if (client.publish("@shadow/data/update", sent_data.c_str())) {
+        lastSentMilllis = CurrentMillis;  // reset timer
+        stateChange = false;  // reset sent flag
+      }
       Serial.print("Sent data: ");
       Serial.println(sent_data);
+      // Serial.println(uxTaskGetStackHighWaterMark(NULL));
     }
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+    vTaskDelay(100/portTICK_PERIOD_MS);
   }
 }
 
@@ -73,13 +123,34 @@ void reconnect() {
     // Attempt to connect
     if (client.connect(clientID, token, secret)) {
       Serial.println("connected");
-      // client.subscribe("esp32/output");
+      client.subscribe("@msg/led");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      vTaskDelay(2000/portTICK_PERIOD_MS);
     }
   }
+}
+
+void IRAM_ATTR PIRtrig(void)
+{
+  portENTER_CRITICAL(&synch);
+  PIRdetected = true;
+  stateChange = true;
+  digitalWrite(2, HIGH);
+  timerRestart(timer0);
+  timerAlarmEnable(timer0);
+  portEXIT_CRITICAL(&synch); 
+}
+
+void IRAM_ATTR onTimer()
+{
+  portENTER_CRITICAL_ISR(&timerMux);
+  PIRdetected = false;
+  stateChange = true;
+  digitalWrite(2, LOW);
+  timerAlarmDisable(timer0);
+  portEXIT_CRITICAL_ISR(&timerMux);
 }
